8ffe08ce51b3798e8ced6a65d9633d0f39997986
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-09 02:15:26 +0300

    feat: add app themes using FlexColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,23 @@
+import 'package:dribbble_challenge/src/onboarding/onboarding_screen.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: DribbleChallenge()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DribbleChallenge extends StatelessWidget {
+  const DribbleChallenge({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: const OnBoardingScreen(),
       theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay),
       themeMode: ThemeMode.dark,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(_counter.toString())
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/src/onboarding/onboarding_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Text("Hello"),
+      ),
+    );
+  }
+}
