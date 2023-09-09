6d804c2684061b7fe17e362d21670b4d58eaae8b
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-09 07:42:20 +0300

    feat: add onboarding screen assets

---
+++ lib/src/core/constants/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+  static const dish = "assets/images/dish.png";
+  static const blackPepper = "assets/images/black_pepper.png";
+  static const leaves = "assets/images/leaves.png";
+}
