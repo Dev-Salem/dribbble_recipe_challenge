c31ef71396cdb22031fc0de48f25ce30087ccc5a
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-11 19:24:13 +0300

    refactor: store app strings in a single class

--- lib/src/core/constants/strings.dart
+++ lib/src/core/constants/strings.dart
@@ -1,1 +1,6 @@
-class Strings {}
+class Strings {
+  static const onBoardingTitle = "Find the perfect recipe";
+  static const onBoardingSlogan =
+      "More than 20 thousand recipes of healthy and delicious food";
+  static const onBoardingButton = "Get Started";
+}
