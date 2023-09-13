abaff607332324f4a3e792c1fcf3a54ac5387d5e
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-13 04:05:53 +0300

    feat: add named routes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dribbble_challenge/src/onboarding/onboarding_screen.dart';
+import 'package:dribbble_challenge/src/recipes/presentation/screens/home_screen.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,7 +13,13 @@ class DribbleChallenge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const OnBoardingScreen(),
+      home: const HomeScreen(),
+      onGenerateRoute: (settings) {
+        return switch (settings.name) {
+          '/' => MaterialPageRoute(builder: (_) => const HomeScreen()),
+          _ => MaterialPageRoute(builder: (_) => const HomeScreen())
+        };
+      },
       theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay)
       //.copyWith(textTheme: GoogleFonts.ubuntuTextTheme())
