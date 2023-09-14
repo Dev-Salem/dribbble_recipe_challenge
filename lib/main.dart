33f8017c3a809722e6255761b52799fe6edd8329
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-14 12:44:36 +0300

    fix: remove google fonts text theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:dribbble_challenge/src/recipes/presentation/screens/home_screen.
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const ProviderScope(child: DribbleChallenge()));
@@ -22,8 +21,7 @@ class DribbleChallenge extends StatelessWidget {
           _ => MaterialPageRoute(builder: (_) => const HomeScreen())
         };
       },
-      theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay)
-          .copyWith(textTheme: GoogleFonts.ubuntuTextTheme()),
+      theme: FlexThemeData.light(scheme: FlexScheme.ebonyClay),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.ebonyClay)
           .copyWith(scaffoldBackgroundColor: const Color(0xff101520)),
       themeMode: ThemeMode.dark,
