0e2ac2b11320e427acee396582b6e5868063559f
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-15 14:03:19 +0300

    fix: add name route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:dribbble_challenge/src/onboarding/onboarding_screen.dart';
+import 'package:dribbble_challenge/src/recipes/domain/recipe.dart';
 import 'package:dribbble_challenge/src/recipes/presentation/screens/home_screen.dart';
+import 'package:dribbble_challenge/src/recipes/presentation/screens/recipe_details_screen.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,10 +16,14 @@ class DribbleChallenge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeScreen(),
+      home: const OnBoardingScreen(),
       onGenerateRoute: (settings) {
         return switch (settings.name) {
           '/' => MaterialPageRoute(builder: (_) => const HomeScreen()),
+          'recipe_details' => MaterialPageRoute(
+              builder: (_) =>
+                  RecipeDetailsScreen(recipe: settings.arguments as Recipe),
+            ),
           _ => MaterialPageRoute(builder: (_) => const HomeScreen())
         };
       },
