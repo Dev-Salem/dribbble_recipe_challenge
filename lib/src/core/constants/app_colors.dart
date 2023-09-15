a44c480f9f95ac538924d217cc73d48c971d81ca
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-15 14:14:34 +0300

    refactor: add app colors class

---
+++ lib/src/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const cardColor = Color(0xff182032);
+  static final bodySmallTextColor = Colors.amberAccent[100];
+  static const scaffoldBackgroundColor = Color(0xff101520);
+  static const infoTextColor = Colors.white60;
+}
