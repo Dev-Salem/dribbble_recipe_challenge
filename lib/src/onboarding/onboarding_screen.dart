acce1679a5e3035fcb3f524f82c7e3a808fa720f
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-26 07:13:01 +0300

    fix: use AnnotatedRegion to fix black app bar

--- lib/src/onboarding/onboarding_screen.dart
+++ lib/src/onboarding/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dribbble_challenge/src/core/widget/annotated_scaffold.dart';
 import 'package:dribbble_challenge/src/onboarding/widgets/onboarding_body_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,7 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(body: OnBoardingBodyWidget()),
-    );
+    return const AnnotatedScaffold(child: OnBoardingBodyWidget());
   }
 }
+//page transition
