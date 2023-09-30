6250ab7bfe0cdf780ee8cf03c77f0ef25e81c341
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-30 08:53:16 +0300

    refactor: trigger ingredient cards using forward(). instead of autoPlay, trigger forward when sliding panel animation is completed

--- lib/src/recipes/presentation/widget/recipe_details/ingredients_card_list.dart
+++ lib/src/recipes/presentation/widget/recipe_details/ingredients_card_list.dart
@@ -1,15 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:dribbble_challenge/src/recipes/domain/recipe.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
+import 'package:dribbble_challenge/src/recipes/domain/recipe.dart';
 import 'package:dribbble_challenge/src/recipes/presentation/widget/recipe_details/ingredient_card_widget.dart';
 
 class IngredientsCardList extends StatelessWidget {
   final Recipe recipe;
+  final Duration delayTime;
+  final Duration slidingDuration;
+  final AnimationController ingredientController;
   const IngredientsCardList({
     Key? key,
     required this.recipe,
+    required this.delayTime,
+    required this.slidingDuration,
+    required this.ingredientController,
   }) : super(key: key);
 
   @override
@@ -18,15 +24,16 @@ class IngredientsCardList extends StatelessWidget {
       height: 50,
       child: ListView.builder(
           itemCount: recipe.ingredients.length,
+          //physics: const ClampingScrollPhysics(),
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) =>
               IngredientCardWidget(ingredient: recipe.ingredients[index])
-                  .animate(delay: 2.seconds)
+                  .animate(controller: ingredientController, autoPlay: false)
                   .scaleXY(
                       begin: 0,
                       end: 1,
                       duration: 500.ms,
-                      delay: (300 * index).ms,
+                      delay: (200 * index).ms,
                       curve: Curves.decelerate)
                   .fadeIn()),
     );
