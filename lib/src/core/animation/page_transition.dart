ea79e1679050a9d3d291e40789a2fc6eacbf09bf
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-30 08:42:21 +0300

    fix: use fade transition when navigating back

--- lib/src/core/animation/page_transition.dart
+++ lib/src/core/animation/page_transition.dart
@@ -8,6 +8,12 @@ class NoAnimationTransition extends MaterialPageRoute {
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation,
       Animation<double> secondaryAnimation, Widget child) {
+    if (animation.status == AnimationStatus.reverse) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    }
     return child;
   }
 }
