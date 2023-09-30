8cc65e8cebd30eeed5eb95ed7695e2a8ac5c9279
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-30 08:55:40 +0300

    refactor: use different icons and names.

--- lib/src/recipes/presentation/widget/home_screen/food_category_widget.dart
+++ lib/src/recipes/presentation/widget/home_screen/food_category_widget.dart
@@ -1,27 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class FoodCategoryWidget extends StatelessWidget {
-  const FoodCategoryWidget({super.key});
+  final String icon;
+  final String name;
+  const FoodCategoryWidget({
+    Key? key,
+    required this.icon,
+    required this.name,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 120,
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.white30, width: 2),
         borderRadius: BorderRadius.circular(25),
       ),
       alignment: Alignment.center,
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text(
-            "🔥",
-            style: TextStyle(fontSize: 20),
+            icon,
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(
+            width: 5,
           ),
           Text(
-            "Popular",
-            style: TextStyle(fontSize: 20),
+            name,
+            style: const TextStyle(fontSize: 20),
           )
         ],
       ),
