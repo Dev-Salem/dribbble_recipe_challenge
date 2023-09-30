41d4dc94defd74f96f14dcbf47ddc07ca2ad081d
Dev Salem <dev.salemsaleh@gmail.com>
2023-09-30 08:47:07 +0300

    fix: add padding and style to ingredient card

--- lib/src/recipes/presentation/widget/recipe_details/ingredient_card_widget.dart
+++ lib/src/recipes/presentation/widget/recipe_details/ingredient_card_widget.dart
@@ -11,12 +11,15 @@ class IngredientCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       alignment: Alignment.center,
       decoration: BoxDecoration(
           border: Border.all(color: Colors.white24, width: 1.5),
           borderRadius: BorderRadius.circular(15)),
-      child: Text(ingredient),
+      child: Text(
+        ingredient,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
